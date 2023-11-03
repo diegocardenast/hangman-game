ca1c5a4646f5000b5772e9a2947660d3d00c6df0
Diego Cardenas <diego.cardenast@gmail.com>
2023-11-03 06:53:38 +0000

    feat: create start_game and select_difficulty functions

--- run.py
+++ run.py
@@ -1,2 +1,51 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
 
+"""
+Game introductory message
+"""
+def start_game():
+    print("===== Welcome to the Hangman Game =====\n")
+    select_difficulty()
+
+"""
+Inspired by https://github.com/makeuseofcode/Hangman-Game/blob/main/hangman.py
+Allows the player to select the difficulty level
+"""
+def select_difficulty():
+    while True:
+        choice = input("Please select the difficulty level (easy/medium/hard): ")
+        
+        if 'easy' in choice.lower():
+            print("You selected easy")
+            print("\n")
+            level = 'easy'
+            get_word_from_list(level)
+            
+            
+        elif 'medium' in choice.lower():
+            print("You selected medium")
+            print("\n")
+            level = 'medium'
+            get_word_from_list(level)
+            
+            
+        elif 'hard' in choice.lower():
+            print("You selected hard")
+            print("\n")
+            level = 'hard'
+            get_word_from_list(level)
+            
+            
+        else:
+            print("Please enter a valid choice.")
+            print("\n")
+
+"""
+Selects a word from a list depending on the difficulty level selected
+"""
+def get_word_from_list(level):
+
+
+
+start_game()
