b33af41f4bb6fd3aa710a28d5276cfe616942c83
Diego Cardenas <diego.cardenast@gmail.com>
2023-11-04 00:07:59 +0000

    feat: adding colorama features to game

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 import random
 import sys
 import os
+from colorama import init
+from colorama import Fore
+from colorama import Back
+from colorama import Style
+init()
 
 
 def start_game():
@@ -9,7 +14,7 @@ def start_game():
     Game introductory message
     """
     os.system('clear')
-    print("===== Welcome to the Hangman Game =====\n")
+    print(Style.BRIGHT + "\n===== Welcome to the Hangman Game =====\n")
     select_difficulty()
 
 
@@ -18,23 +23,23 @@ def select_difficulty():
     Allows the player to select the difficulty level
     """
     while True:
-        choice = input("Select easy/medium/hard level or 'instructions':\n")
+        choice = input(Style.NORMAL + "Write 'easy', 'medium' or 'hard' level, or 'instructions':\n")
 
         if 'easy' in choice.lower():
-            print("You selected easy")
-            print("\n")
+            print(Fore.BLUE + "You selected easy")
+            print(Fore.RESET + "\n")
             level = 'easy'
             get_word_from_list(level)
 
         elif 'medium' in choice.lower():
-            print("You selected medium")
-            print("\n")
+            print(Fore.YELLOW + "You selected medium")
+            print(Fore.RESET + "\n")
             level = 'medium'
             get_word_from_list(level)
 
         elif 'hard' in choice.lower():
-            print("You selected hard")
-            print("\n")
+            print(Fore.RED + "You selected hard")
+            print(Fore.RESET + "\n")
             level = 'hard'
             get_word_from_list(level)
         elif 'instructions' in choice.lower():
@@ -78,61 +83,68 @@ def draw_hangman(chances):
         print(" |             ")
         print(" |             ")
         print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Back.WHITE + Fore.WHITE + "¯¯¯¯¯          ")
+        print(Back.RESET + "\n")
     elif chances == 5:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |             ")
-        print(" |             ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.CYAN + " ________      ")
+        print(Fore.CYAN + " |      |      ")
+        print(Fore.CYAN + " |      0      ")
+        print(Fore.CYAN + " |             ")
+        print(Fore.CYAN + " |             ")
+        print(Fore.CYAN + " |             ")
+        print(Back.CYAN + Fore.CYAN + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
     elif chances == 4:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |     /       ")
-        print(" |             ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.BLUE + " ________      ")
+        print(Fore.BLUE + " |      |      ")
+        print(Fore.BLUE + " |      0      ")
+        print(Fore.BLUE + " |     /       ")
+        print(Fore.BLUE + " |             ")
+        print(Fore.BLUE + " |             ")
+        print(Back.BLUE + Fore.BLUE + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
     elif chances == 3:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |     /|      ")
-        print(" |             ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.YELLOW + " ________      ")
+        print(Fore.YELLOW + " |      |      ")
+        print(Fore.YELLOW + " |      0      ")
+        print(Fore.YELLOW + " |     /|      ")
+        print(Fore.YELLOW + " |             ")
+        print(Fore.YELLOW + " |             ")
+        print(Back.YELLOW + Fore.YELLOW + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
     elif chances == 2:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |     /|\\    ")
-        print(" |             ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.MAGENTA + " ________      ")
+        print(Fore.MAGENTA + " |      |      ")
+        print(Fore.MAGENTA + " |      0      ")
+        print(Fore.MAGENTA + " |     /|\\    ")
+        print(Fore.MAGENTA + " |             ")
+        print(Fore.MAGENTA + " |             ")
+        print(Back.MAGENTA + Fore.MAGENTA + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
     elif chances == 1:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |     /|\\    ")
-        print(" |     /       ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.RED + " ________      ")
+        print(Fore.RED + " |      |      ")
+        print(Fore.RED + " |      0      ")
+        print(Fore.RED + " |     /|\\    ")
+        print(Fore.RED + " |     /       ")
+        print(Fore.RED + " |             ")
+        print(Back.RED + Fore.RED + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
     elif chances == 0:
         os.system('clear')
-        print(" ________      ")
-        print(" |      |      ")
-        print(" |      0      ")
-        print(" |     /|\\    ")
-        print(" |     / \\    ")
-        print(" |             ")
-        print("¯¯¯¯¯          ")
+        print(Fore.RED + " ________      ")
+        print(Fore.RED + " |      |      ")
+        print(Fore.RED + " |      0      ")
+        print(Fore.RED + " |     /|\\    ")
+        print(Fore.RED + " |     / \\    ")
+        print(Fore.RED + " |             ")
+        print(Back.RED + Fore.RED + "¯¯¯¯¯          ")
+        print(Back.RESET + Fore.RESET + "\n")
 
 
 def run_game(word):
@@ -145,37 +157,39 @@ def run_game(word):
     found = False
     while True:
         if chances == 0:
-            print(f"Game Over! The word was: {word}")
-            gameOver_choice = input("Do you want to play again? (yes/no):\n")
-            while True:
+            print(Fore.RED + f"Game Over! The word was: {word}")
+            gameOver_check = True
+            while gameOver_check == True:
+                gameOver_choice = input(Fore.RESET + "Do you want to play again? (Write 'yes' or 'no'):\n")
                 if 'yes' in gameOver_choice.lower():
+                    gameOver_check = False
                     start_game()
                 elif 'no' in gameOver_choice.lower():
                     print('Quitting the game...')
+                    gameOver_check = False
                     sys.exit()
                 else:
                     os.system('clear')
                     print("Please enter a valid choice.")
                     print("\n")
-
-        print("==== Guess the word ====")
-        print(temp, end='')
+        print(Style.BRIGHT + "==== Guess the word ====\n")
+        print(Style.NORMAL + temp, end='')
         print(f"\t(word has {len(word)} letters)")
         print(f"Chances left: {chances}")
-        character = input("Enter a letter ('restart' or 'instructions'):\n")
+        character = input("Write a letter, 'restart' or 'instructions':\n")
         if len(character) > 1 or not character.isalpha():
-            if (character == "restart"):
+            if ("restart" in character.lower()):
                 start_game()
-            elif (character == "instructions"):
+            elif ("instructions" in character.lower()):
                 check_instructions()
                 continue
             else:
                 os.system('clear')
-                print("Enter a single letter only or 'restart' to start again")
+                print("Write a single letter only or 'restart' to start again")
                 continue
         else:
             for num, char in enumerate(list(word)):
-                if char == character:
+                if char == character.lower():
                     templist = list(temp)
                     templist[num] = char
                     temp = ''.join(templist)
@@ -185,17 +199,20 @@ def run_game(word):
         else:
             chances -= 1
         if '_' not in temp:
-            print(f"\nYou Won! The word was: {word}")
-            gameWon_choice = input("Do you want to play again? (yes/no):\n")
-            while True:
+            print(Fore.GREEN + f"\nYou Won! The word was: {word}")
+            gameWon_check = True
+            while gameWon_check == True:
+                gameWon_choice = input(Fore.RESET + "Do you want to play again? (Write 'yes' or 'no'):\n")
                 if 'yes' in gameWon_choice.lower():
+                    gameWon_check = False
                     start_game()
                 elif 'no' in gameWon_choice.lower():
                     print('Quitting the game...')
+                    gameWon_check = False
                     sys.exit()
                 else:
                     os.system('clear')
-                    print("Please enter a valid choice.")
+                    print("Please write a valid choice.")
                     print("\n")
         else:
             draw_hangman(chances)
@@ -207,10 +224,10 @@ def check_instructions():
     Game Instructions
     """
     os.system('clear')
-    print("INSTRUCTIONS:")
-    print("- The goal of the game is to guess the mysterious word")
+    print(Back.CYAN + Style.BRIGHT + "INSTRUCTIONS:")
+    print(Back.RESET + Style.NORMAL + "- The goal of the game is to guess the mysterious word")
     print("before the man is hanged.")
-    print("- Select one of the 3 difficulty levels (easy, medium or hard).")
+    print("- Write one of the 3 difficulty levels (easy, medium or hard).")
     print("The difference is the lenght of the words.")
     print("- You can guess only one letter at a time.")
     print("- You have 7 opportunities to make mistakes.")
